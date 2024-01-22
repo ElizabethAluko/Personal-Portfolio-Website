af2f1d46150458a9c0b185ca0164bb4c24e6c2d0
ElizabethAluko <jolaoyebeth24@gmail.com>
2024-01-22 19:57:48 +0100

    Add content

--- script.js
+++ script.js
@@ -39,3 +39,9 @@ balls.forEach((el, i, ra) => {
     }
   );
 });
+
+document.getElementById('menu-icon').addEventListener('click', function () {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+    this.innerHTML = menu.classList.contains('active') ? '&#10006;' : '&#9776;';
+});
